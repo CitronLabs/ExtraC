08c0d799dd7ac321d7398a55e489979fb8b391ec
blueberry <blueberryprograms@gmail.com>
2025-12-08 02:36:27 -0500

    Implementing updated StringBuilder class and utils for Strings package

--- lib/Core/Strings/StringBuilder/StringBuilder.c
+++ lib/Core/Strings/StringBuilder/StringBuilder.c
@@ -3,114 +3,14 @@
 import(std)
 
 
-errvt moduleMethod(std_StringBuilder, Max, u64 max_len){
-	nonull(self, return err)
-	std.List.Limit(priv.data, max_len);
-return OK;
-}
+errvt moduleMethod(std_StringBuilder,   Max,     u64 max_len);
+len_t moduleMethod(std_StringBuilder,   Set,     std_String* string);
+len_t moduleMethod(std_StringBuilder,   Append,  std_String* string);
+len_t moduleMethod(std_StringBuilder,   Prepend, std_String* string);
+len_t moduleMethod(std_StringBuilder,   Insert,  u64 index, std_String* string);
+errvt moduleMethod(std_StringBuilder,   Clear);
 
-u64 moduleMethod(std_StringBuilder, Set, va_list args, ...){
-	nonull(self, return 0);
-
-	u64 len = 0;
-	
-	std.List.Flush(priv.data);
-
-	if(!args) { va_start(args, args); }
-
-	len = std.fmt.print.varArgs(priv.in_stream, args);
-	
-	va_end(args);
-
-	if(!len){ std.Stream.Flush(priv.in_stream); return 0; }
-	
-	std.List.Reserve(priv.data, RESERVE_ATLEAST, elements(priv.in_stream));
-
-	// TODO(blueberry): finish this
-	
-return len;
-}
-u64 moduleMethod(std_StringBuilder, Append, va_list args, ...){
-	
-	nonull(self){ return 0; }
-
-	u64 len = 0;
-
-	if(string != null){
-		std.List.Append(priv.data, string->txt.data.utf8, string->__private->len_bytes);
-		len = string->len;
-	}else{
-
-		va_list args;
-		va_start(args, string);
-
-		len = FormatUtils.FormatVArgs(self, args);   
-	
-		va_end(args);
-	}
-return len;
-}
-u64 moduleMethod(std_StringBuilder, Prepend, va_list args, ...){
-	nonull(self, return 0);
-	u64 len = 0;
-	
-
-	var builder = push(std_StringBuilder, priv.ctype);
-
-		va_list args;
-		va_start(args, string);
-	
-		len = FormatUtils.FormatVArgs(builder, args);   
-		
-		va_end(args);
-
-		stringToAdd = std_StringBuilder.GetStr(builder);
-		string = &stringToAdd;
-
-	std.List.Insert(priv.data, string->__private->len_bytes, 0, string->txt.data.utf8);
-	len = string->len;
-
-return len;
-}
-u64 moduleMethod(std_StringBuilder, Insert, u64 index, va_list args, ...){
-	
-	nonull(priv, return 0);
-	u64 len = 0;
-	
-	inst builder = null; 
-	data(String) stringToAdd;
-
-	if(string == null){
-
-		builder = push(std_StringBuilder, priv.ctype);
-
-		va_list args;
-		va_start(args, string);
-	
-		len = FormatUtils.FormatVArgs(builder, args);   
-		
-		va_end(args);
-
-		stringToAdd = std_StringBuilder.GetStr(builder);
-		string = &stringToAdd;
-	}
-		
-	std.List.Insert(priv.data, string->__private->len_bytes, index, string->txt.data.utf8);
-
-	priv.len += string->len;
-
-	if(builder) pop(builder);
-
-return string->len;
-}
-
-errvt moduleMethod(std_StringBuilder, Clear){
-	nonull(self){ return err; }
-	std.List.Flush(priv.data);
-return OK;
-}
-
-String_Instance moduleMethod(std_StringBuilder, GetStr){
+std_String* moduleMethod(std_StringBuilder, GetStr){
 	nonull(self, return (String_Instance){0});
 	
 	priv.interalStrPriv.len_bytes = std.List.Size(priv.data);
@@ -128,63 +28,26 @@ String_Instance moduleMethod(std_StringBuilder, GetStr){
 return result;
 }
 
-inst(String) moduleMethod(std_StringBuilder, CreateStr){
+std_String* moduleMethod(std_StringBuilder, CreateStr){
 	nonull(self){ return nil; }
 
 	u64 len = std.List.Size(priv.data);
 
-return 
-	initialize(String, calloc(len + 1, sizeof(u8)),
-	    .txt = {
-		.type = priv.ctype,
-     		.data.utf8 = std.List.GetPointer(priv.data, 0)
-	    },
-	    .bytes_len = len,
-	    .inline_alloc = true
-	);
-}
-
-u64 moduleIMethod(std_StringBuilder, Print, FormatID* formats, inst(std_StringBuilder) out){
-	self(std_StringBuilder);
-
-	std.List.Append(priv.data, "\0", 1);
-	
-	u64 formated_len = std_StringBuilder.Append(out, null, 
-			"(std_StringBuilder){ max_len = ",$(priv.max_len),", str = \"", 
-	endstr);
-	data(String) internalString = std_StringBuilder.GetStr(self);
-	formated_len += std_StringBuilder.Append(out, &internalString);
-	
-	formated_len += std_StringBuilder.Append(out, s("\" }"));
-
-	std.List.Pop(priv.data, 1);
-return formated_len;
-}
-
-u64 moduleIMethod(std_StringBuilder, Scan, FormatID* formats, inst(String) in){
-	self(std_StringBuilder);
-
-return std_StringBuilder.Append(self, in);
-}
-
-errvt moduleIMethod(std_StringBuilder, Free){
-	self(std_StringBuilder);
-	nonull(self){ return err; }
-	del(priv.data);
-return OK;
 }
 
 construct(std_StringBuilder,
+FMT(),
+DEF(),
 ){
 
 	if(arg.type >= CHAR_INVALID)
 	  	return nil;
 
-	setpriv(std_StringBuilder){
-		.data = newList(u8,  arg.init_str ? 20 : args->init_str->len),
-		.max_len = arg.limit == 0 ? UINT64_MAX : args->limit,
-	  	.ctype = arg.type,
-	};
+	
+	priv.data 		= newList(u8,  arg.init_str ? 20 : args->init_str->len),
+	priv.max_len 		= arg.limit == 0 ? maxof(len_t) : args->limit,
+	priv.ctype 		= arg.type,
+	
 
 	std.List.Limit(priv.data, priv.max_len);
 	
--- lib/Core/Strings/StringBuilder/pkg.h
+++ lib/Core/Strings/StringBuilder/pkg.h
@@ -9,9 +9,8 @@
 Decl(List)
 
 Class(StringBuilder,
-INIT(std_char_type type; std_String* init_str; u64 limit;),
+INIT(std_String* init_str; u64 limit),
 FMT(),
-
 private(
 	std_List* data;
 	std_Stream* in_stream;
@@ -23,98 +22,14 @@ private(
       	std_StreamEncoder streamEncoder;
       	std_StreamDecoder streamDecoder;
 
-	/**
-	 * @brief Sets a maximum length for the StringBuilder.
-	 * @details This function limits the total number of characters that the
-	 * StringBuilder can hold. Any subsequent append, prepend, or insert
-	 * operations will respect this limit.
-	 *
-	 * @param self A pointer to the `StringBuilder` instance.
-	 * @param max_len The maximum length in characters.
-	 * @return An `errvt` (error value) indicating success or failure.
-	 */
-	errvt method(StringBuilder, Max, u64 max_len);
+	errvt method(StringBuilder,   Max,     u64 max_len);
+	len_t method(StringBuilder,   Set,     std_String* string);
+	len_t method(StringBuilder,   Append,  std_String* string);
+	len_t method(StringBuilder,   Prepend, std_String* string);
+	len_t method(StringBuilder,   Insert,  u64 index, std_String* string);
+	errvt method(StringBuilder,   Clear);
 	
-	/**
-	 * @brief Sets the content of the StringBuilder.
-	 * @details This function replaces the existing content of the StringBuilder
-	 * with the provided string. It can accept either a `String` instance or
-	 * a variable number of formatted arguments.
-	 *
-	 * @param self A pointer to the `StringBuilder` instance.
-	 * @param arg va_list of format args used to set, or `null` if using variable arguments.
-	 * @param ... A variable number of arguments to be formatted and set as the content.
-	 * @return The new length of the StringBuilder in characters.
-	 */
-	u64 method(StringBuilder, Set, __);
-	
-	/**
-	 * @brief Appends a string or formatted content to the StringBuilder.
-	 * @details This function adds new content to the end of the StringBuilder's
-	 * current content. It can accept a `String` instance or variable formatted arguments.
-	 *
-	 * @param self A pointer to the `StringBuilder` instance.
-	 * @param arg va_list of format args used to append, or `null` if using variable arguments.
-	 * @param ... A variable number of arguments to be formatted and appended.
-	 * @return The number of characters appended.
-	 */
-	u64 method(StringBuilder, Append, __);
-	
-	/**
-	 * @brief Prepends a string or formatted content to the StringBuilder.
-	 * @details This function adds new content to the beginning of the StringBuilder's
-	 * current content. It can accept a `String` instance or variable formatted arguments.
-	 *
-	 * @param self A pointer to the `StringBuilder` instance.
-	 * @param arg va_list of format args used to prepend, or `null` if using variable arguments.
-	 * @param ... A variable number of arguments to be formatted and prepended.
-	 * @return The number of characters prepended.
-	 */
-	u64 method(StringBuilder, Prepend, __);
-	
-	/**
-	 * @brief Inserts a string or formatted content at a specified index.
-	 * @details This function inserts new content into the StringBuilder at a specific
-	 * character index. It can accept a `String` instance or variable formatted arguments.
-	 *
-	 * @param self A pointer to the `StringBuilder` instance.
-	 * @param index The character index at which to insert the new content.
-	 * @param arg va_list of format args used to insert, or `null` if using variable arguments.
-	 * @param ... A variable number of arguments to be formatted and inserted.
-	 * @return The number of characters inserted.
-	 */
-	u64 method(StringBuilder, Insert, u64 index, __);
-	
-
-	/**
-	 * @brief Clears the content of the StringBuilder.
-	 * @details This function empties the StringBuilder, resetting its length to zero
-	 * and freeing any allocated data.
-	 *
-	 * @param self A pointer to the `StringBuilder` instance.
-	 * @return An `errvt` (error value) indicating success or failure.
-	 */
-	errvt method(StringBuilder, Clear);
-	
-	/**
-	 * @brief Retrieves the current content of the StringBuilder as a `String_Instance`.
-	 * @details This function returns a copy of the StringBuilder's content in the
-	 * form of a `String_Instance`. It does not create a new `String` object on the heap.
-	 *
-	 * @param self A pointer to the `StringBuilder` instance.
-	 * @return A `String_Instance` containing the builder's current content.
-	 */
 	std_String* method(StringBuilder, GetStr);
-	
-	/**
-	 * @brief Creates a new `String` instance from the StringBuilder's content.
-	 * @details This function allocates a new `String` object on the heap and copies
-	 * the StringBuilder's content into it. The returned `String` must be freed
-	 * by the caller.
-	 *
-	 * @param self A pointer to the `StringBuilder` instance.
-	 * @return A new `String` instance, or `null` on failure.
-	 */
 	std_String* method(StringBuilder, CreateStr);
 
 };
