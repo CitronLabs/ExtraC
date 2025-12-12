a731c9621f76bc603ac24f725393a7b8ced2ffd4
blueberry <blueberryprograms@gmail.com>
2025-12-11 19:31:49 -0500

    Defined std.Math.Constants package

--- lib/Core/Math/Constants/pkg.h
+++ lib/Core/Math/Constants/pkg.h
@@ -2,9 +2,38 @@
 #define __XC_MATH__
 #include "../pkg.h"
 
+#define package std
+	Decl(Number)
+
+#undef package
 #define package std_Math
 
+typefrom(word, Constant_Type);
+	
 Interface(Constants,
+  values(Value, double,
+	PI, 			
+	EULAR,			
+	GOLDEN_RATIO,
+	EULER_MASCH, 		
+	PYTHAGORAS, 		
+	NLOGOF2, 		
+	GAUSS, 			
+	GLAISHER_KINKELIN,
+	APERY 			
+  )
+  values(Type, std_Math_Constant_Type,
+	PI, 			
+	EULAR,			
+	GOLDEN_RATIO,
+	EULER_MASCH, 		
+	PYTHAGORAS, 		
+	NLOGOF2, 		
+	GAUSS, 			
+	GLAISHER_KINKELIN,
+	APERY 			
+  )
+	std_Number* fn(calculate)(std_Number* num, std_Math_Constant_Type type, len_t places);
 )
 
 #undef package
